d44ed659cb7ff774d7c1c27c9cbea55e5f8b8fb9
James Swank <jmswank7@gmail.com>
2023-05-02 17:59:20 -0600

    gets data

--- app.py
+++ app.py
@@ -2,7 +2,8 @@ import requests
 from dash import Dash, html, dcc, Input, Output
 import dash_bootstrap_components as dbc
 import dash_auth
-from users import VALID_USERNAME_PASSWORD_PAIRS
+import pandas as pd
+from users import VALID_USERNAME_PASSWORD_PAIRS, username, password
 
 
 app = Dash(__name__, suppress_callback_exceptions=True, external_stylesheets=[dbc.themes.DARKLY])
@@ -12,9 +13,19 @@ auth = dash_auth.BasicAuth(
     VALID_USERNAME_PASSWORD_PAIRS
 )
 
+username= username
+password=password
+print(username)
+
 header = html.Div("NSSP DATA", className="h2 p-2 text-white bg-primary text-center")
 
+URL = "https://essence.syndromicsurveillance.org/nssp_essence/api/timeSeries?endDate=9Feb2021&medicalGrouping=injury&percentParam=noPercent&geographySystem=hospitaldhhsregion&datasource=va_hospdreg&detector=probrepswitch&startDate=11Nov2020&timeResolution=daily&medicalGroupingSystem=essencesyndromes&userId=455&aqtTarget=TimeSeries"
 
+r = requests.get(url=URL, auth=(username, password))
+data = r.json()
+print(type(data))
+df = pd.DataFrame.from_dict(data)
+print(df)
 
 app.layout = dbc.Container(
     [
@@ -26,6 +37,5 @@ app.layout = dbc.Container(
 if __name__ == '__main__':
     app.run_server(debug=True)
 
-# url = "https://essence.syndromicsurveillance.org/nssp_essence/api/timeSeries?endDate=9Feb2021&medicalGrouping=injury&percentParam=noPercent&geographySystem=hospitaldhhsregion&datasource=va_hospdreg&detector=probrepswitch&startDate=11Nov2020&timeResolution=daily&medicalGroupingSystem=essencesyndromes&userId=455&aqtTarget=TimeSeries"
 
 # r = requests.get(url=URL, auth=(username, password))
