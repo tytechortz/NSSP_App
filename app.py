4077e31c38760f28eeb79ebd1d998d745a8c4c81
James Swank <jmswank7@gmail.com>
2023-05-02 17:01:22 -0600

    starts new layout

--- app.py
+++ app.py
@@ -4,7 +4,6 @@ import dash_bootstrap_components as dbc
 import dash_auth
 from users import VALID_USERNAME_PASSWORD_PAIRS
 
-# external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = Dash(__name__, suppress_callback_exceptions=True, external_stylesheets=[dbc.themes.DARKLY])
 
@@ -12,31 +11,17 @@ auth = dash_auth.BasicAuth(
     app,
     VALID_USERNAME_PASSWORD_PAIRS
 )
-print(VALID_USERNAME_PASSWORD_PAIRS)
 
-app.layout = html.Div([
-    html.H1('Welcome to the app'),
-    html.H3('You are successfully authorized'),
-    dcc.Dropdown(['A', 'B'], 'A', id='dropdown'),
-    dcc.Graph(id='graph')
-], className='container')
+header = html.Div("NSSP DATA", className="h2 p-2 text-white bg-primary text-center")
 
-@app.callback(
-    Output('graph', 'figure'),
-    [Input('dropdown', 'value')])
-def update_graph(dropdown_value):
-    return {
-        'layout': {
-            'title': 'Graph of {}'.format(dropdown_value),
-            'margin': {
-                'l': 20,
-                'b': 20,
-                'r': 10,
-                't': 60
-            }
-        },
-        'data': [{'x': [1, 2, 3], 'y': [4, 1, 2]}]
-    }
+
+
+app.layout = dbc.Container(
+    [
+        header,
+    ],
+)
+
 
 if __name__ == '__main__':
     app.run_server(debug=True)
