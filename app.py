96fbd2c14ac3cd07ca34bfc0f75370432f0d3a2f
James Swank <jmswank7@gmail.com>
2023-05-02 16:51:53 -0600

    starts app

--- app.py
+++ app.py
@@ -1,2 +1,46 @@
 import requests
+from dash import Dash, html, dcc, Input, Output
+import dash_bootstrap_components as dbc
+import dash_auth
+from users import VALID_USERNAME_PASSWORD_PAIRS
 
+# external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+
+app = Dash(__name__, suppress_callback_exceptions=True, external_stylesheets=[dbc.themes.DARKLY])
+
+auth = dash_auth.BasicAuth(
+    app,
+    VALID_USERNAME_PASSWORD_PAIRS
+)
+print(VALID_USERNAME_PASSWORD_PAIRS)
+
+app.layout = html.Div([
+    html.H1('Welcome to the app'),
+    html.H3('You are successfully authorized'),
+    dcc.Dropdown(['A', 'B'], 'A', id='dropdown'),
+    dcc.Graph(id='graph')
+], className='container')
+
+@app.callback(
+    Output('graph', 'figure'),
+    [Input('dropdown', 'value')])
+def update_graph(dropdown_value):
+    return {
+        'layout': {
+            'title': 'Graph of {}'.format(dropdown_value),
+            'margin': {
+                'l': 20,
+                'b': 20,
+                'r': 10,
+                't': 60
+            }
+        },
+        'data': [{'x': [1, 2, 3], 'y': [4, 1, 2]}]
+    }
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
+
+# url = "https://essence.syndromicsurveillance.org/nssp_essence/api/timeSeries?endDate=9Feb2021&medicalGrouping=injury&percentParam=noPercent&geographySystem=hospitaldhhsregion&datasource=va_hospdreg&detector=probrepswitch&startDate=11Nov2020&timeResolution=daily&medicalGroupingSystem=essencesyndromes&userId=455&aqtTarget=TimeSeries"
+
+# r = requests.get(url=URL, auth=(username, password))
