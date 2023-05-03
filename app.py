23ae11c5fb30a9a8f39b64429d8fe0465df8fa2e
James Swank <jmswank7@gmail.com>
2023-05-02 19:07:49 -0600

    gets data into dataframe

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from dash import Dash, html, dcc, Input, Output
 import dash_bootstrap_components as dbc
 import dash_auth
 import pandas as pd
+from io import StringIO
 from users import VALID_USERNAME_PASSWORD_PAIRS, username, password
 
 
@@ -22,9 +23,23 @@ header = html.Div("NSSP DATA", className="h2 p-2 text-white bg-primary text-cent
 URL = "https://essence.syndromicsurveillance.org/nssp_essence/api/timeSeries?endDate=9Feb2021&medicalGrouping=injury&percentParam=noPercent&geographySystem=hospitaldhhsregion&datasource=va_hospdreg&detector=probrepswitch&startDate=11Nov2020&timeResolution=daily&medicalGroupingSystem=essencesyndromes&userId=455&aqtTarget=TimeSeries"
 
 r = requests.get(url=URL, auth=(username, password))
+print(type(r))
+
 data = r.json()
-print(type(data))
-df = pd.DataFrame.from_dict(data)
+print(len(data))
+keys = data.keys()
+for key in keys:
+    print(key)
+
+print(data['timeSeriesData'][0]['count'])
+
+df = pd.DataFrame.from_dict(data['timeSeriesData'])
+df.drop(['details', 'altText'], axis=1, inplace=True)
+# print(type(data))
+# df = pd.DataFrame.from_dict(data)
+# df = pd.DataFrame(r['data'])
+# df = pd.read_csv(StringIO(r), sep=',')
+# df = pd.read_csv(URL)
 print(df)
 
 app.layout = dbc.Container(
